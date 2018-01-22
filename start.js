da795ecf76f4218141df1e4666fc0b05593e52ea
lore-masc <lorenzo.masciullo@studenti.unitn.it>
2018-01-22 17:57:35 +0100

    enable cors for all kind of HTTP requersts

--- start.js
+++ start.js
@@ -20,8 +20,10 @@ app.use(bodyParser.json());
     Eg, Access-Control-Allow-Origin: 'mywebsite.com'
 */
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "content-type");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
 	next();
 });
 
